80a8e7ca3655a38e3fea0cb00758c1485b2c766e
barabas007 <shop.ru.shop@mail.ru>
2021-10-12 19:17:51 +0300

    Stateless

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,68 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp( // Cupertino 
+  runApp(MyFirstClass());
+}
+
+class MyFirstClass extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.indigo,
         appBar: AppBar(
-          title: Text("My first App"),
+          title: Text("My First App"),
           centerTitle: true,
         ),
         body: Center(
-          child: Text("This is my Homepage",
-          style: TextStyle(
-            fontSize: 30,
-            fontWeight: FontWeight.bold,
-            color: Colors.white
-           ),
-         ),
-       ),
-     ),
-    ),
-  );
+          child: Container(
+            padding: EdgeInsets.all(16),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                LinearProgressIndicator(value: 23),
+                Text(
+                  '23%',
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+                Text(
+                  'Press button to download',
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+              ],
+            ),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: null,
+          child: Icon(Icons.cloud_download),
+          ),
+      ),
+    );
+  }
 }
+  
+//   runApp(
+//     MaterialApp( // Cupertino 
+//       home: Scaffold(
+//         backgroundColor: Colors.indigo,
+//         appBar: AppBar(
+//           title: Text("My first App"),
+//           centerTitle: true,
+//         ),
+//         body: Center(
+//           child: Text("This is my Homepage",
+//           style: TextStyle(
+//             fontSize: 30,
+//             fontWeight: FontWeight.bold,
+//             color: Colors.white
+//            ),
+//          ),
+//        ),
+//      ),
+//     ),
+//   );
+ 
 
 
 
